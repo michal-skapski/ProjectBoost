5a9139e014192de02ac2da7006a868e91ac045df
MichalSkapski <mskapski@edu.cdv.pl>
2020-08-12 22:12:16 +0200

    Addinng prefabs

---
+++ Assets/Scripts/Rocket.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    private Rigidbody _rb;
+    private void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.Space)) // can thrust while rotating
+        {
+            
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            
+        }
+    }
+
+    private void Awake()
+    {
+        _rb = _rb.GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        ProcessInput();
+    }
+}
